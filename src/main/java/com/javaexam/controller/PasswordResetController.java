cf1dc07ac7d21a16f69e6b0d11995d2258728e4c
rio-kawabata <7.23.rio.k@gmail.com>
2026-04-01 15:44:03 +0900

    Controllerとdtoを作成

---
+++ src/main/java/com/javaexam/controller/PasswordResetController.java
@@ -0,0 +1,54 @@
+package com.javaexam.controller;
+
+import com.javaexam.dto.PasswordResetRequestDto;
+import com.javaexam.dto.PasswordResetDto;
+import com.javaexam.service.PasswordResetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/password")
+public class PasswordResetController {
+
+    private final PasswordResetService passwordResetService;
+
+    public PasswordResetController(PasswordResetService passwordResetServise) {
+        this.passwordResetService = passwordResetServise;
+    }
+
+    @GetMapping("/forgot")
+    public String forgotPage() {
+        return "forgot-password";
+    }
+
+    @PostMapping("/forgot")
+    public String handleForgot(@ModelAttribute  PasswordResetRequestDto dto,
+        Model model) {
+            try {
+                passwordResetService.createToken(dto.getUserId());
+                return "redirect:/password/sent";
+            } catch (RuntimeException e) {
+                model.addAttribute("error", e.getMessage());
+                return "forgot-password";
+            }
+        }
+    
+    @GetMapping("/sent")
+    public String sentPage() {
+        return "password-reset-sent";
+    }
+
+   @GetMapping("/reset")
+    public String resetPage(@RequestParam String token, Model model) {
+        model.addAttribute("token", token);
+        return "reset-password";
+    }
+
+    @PostMapping("/reset")
+    public String handleReset(@ModelAttribute PasswordResetDto dto) {
+        passwordResetService.resetPassword(dto.getToken(), dto.getNewPassword());
+        return "redirect:/login";
+    }   
+}
+
---
+++ src/main/java/com/javaexam/dto/PasswordResetRequestDto.java
@@ -0,0 +1,9 @@
+package com.javaexam.dto;
+
+public class PasswordResetRequestDto {
+    private String userId;
+
+    public String getUserId() { return userId;}
+    public void setUserId(String userId) { this.userId = userId; }
+    
+}
